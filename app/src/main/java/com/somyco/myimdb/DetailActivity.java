0d3b11214054dff7c6cec0931f240a78c96a6ef9
Nicolas Mas <nicolas.mas@gmail.com>
2015-02-23 11:15:21 +0800

    Added the second call to get the details by ID on item clicked in the list view Prep for the detail view

---
+++ app/src/main/java/com/somyco/myimdb/DetailActivity.java
@@ -0,0 +1,29 @@
+package com.somyco.myimdb;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.TextureView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by nicolasmas on 23/2/15.
+ */
+public class DetailActivity extends ActionBarActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Tell the activity which XML layout is right
+        setContentView(R.layout.detail_movie);
+
+        // Enable the "Up" button for more navigation options
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Access the imageview from XML
+        ImageView imageView = (ImageView) findViewById(R.id.detail_movie_poster);
+        TextView  titleView = (TextView) findViewById(R.id.detail_text_title);
+        TextView  yearView = (TextView) findViewById(R.id.detail_text_year);
+    }
+}
--- app/src/main/java/com/somyco/myimdb/MainActivity.java
+++ app/src/main/java/com/somyco/myimdb/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -21,7 +22,7 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
 
-public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener,AdapterView.OnItemClickListener {
 
     // Start of custom vars
     public TextView m_movie_field;
@@ -48,7 +49,7 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
 
         // ListView linkage
         mainListView = (ListView) findViewById(R.id.main_listview);
-        // TODO add an action listener
+        mainListView.setOnItemClickListener(this);
 
         // movie to search linkage with m_movie_field
         m_movie_field = (TextView)findViewById(R.id.input_movie_title);
@@ -88,8 +89,33 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         // Let's just pop an animation for now..
-        Toast.makeText(this, "Button test", Toast.LENGTH_LONG).show();
+        //Toast.makeText(this, "Button test", Toast.LENGTH_LONG).show();
         getMovie(m_movie_field.getText().toString());
+    }
+
+    /**
+     * Callback method to be invoked when an item in this AdapterView has
+     * been clicked.
+     * <p/>
+     * Implementers can call getItemAtPosition(position) if they need
+     * to access the data associated with the selected item.
+     *
+     * @param parent   The AdapterView where the click happened.
+     * @param view     The view within the AdapterView that was clicked (this
+     *                 will be a view provided by the adapter)
+     * @param position The position of the view in the adapter.
+     * @param id       The row id of the item that was clicked.
+     */
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        // Let's get the id of the movie for details request
+        JSONObject jsonObject = (JSONObject) mJSONAdapter.getItem(position);
+        Toast.makeText(this, "detail requested: " + jsonObject.toString(), Toast.LENGTH_LONG).show();
+
+        // Retrieving the details for the specific movie
+        Toast.makeText(this, "detail IMDBID: " + jsonObject.optString("imdbID".toString()), Toast.LENGTH_LONG).show();
+        this.getMovieDetailIMDB(jsonObject.optString("imdbID"));
     }
 
 
@@ -113,24 +139,68 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         // Let's display the progress bar right after we created the http call
         m_Dialog.show();
         m_asynclient.get(m_url, new JsonHttpResponseHandler(){
-            @Override
-            public void onSuccess(JSONObject jsonObject) {
+                    @Override
+                    public void onSuccess(JSONObject jsonObject) {
 
-                //Toast.makeText(getApplicationContext(), "Success!", Toast.LENGTH_LONG).show();
-                //Log.d("myimdb", jsonObject.toString());
+                        //Toast.makeText(getApplicationContext(), "Success!", Toast.LENGTH_LONG).show();
+                        //Log.d("myimdb", jsonObject.toString());
 
-                // update the data in your custom method.
-                m_Dialog.dismiss();
-                mJSONAdapter.updateData(jsonObject.optJSONArray("Search"));
-            }
+                        // update the data in your custom method.
+                        m_Dialog.dismiss();
+                        mJSONAdapter.updateData(jsonObject.optJSONArray("Search"));
+                    }
 
-            @Override
-            public void onFailure(int statusCode, Throwable throwable, JSONObject error) {
+                    @Override
+                    public void onFailure(int statusCode, Throwable throwable, JSONObject error) {
 
-                Toast.makeText(getApplicationContext(), "failed with!"+throwable.getMessage(), Toast.LENGTH_LONG).show();
-                Log.e("myimdb", statusCode + " " + throwable.getMessage());
-            }
+                        Toast.makeText(getApplicationContext(), "failed with!"+throwable.getMessage(), Toast.LENGTH_LONG).show();
+                        Log.e("myimdb", statusCode + " " + throwable.getMessage());
+                    }
+                }
+
+        );
+
+
+    }
+
+    private void getMovieDetailIMDB(String imdbID)
+    {
+        String m_url = "";
+        String m_string;
+        try{
+            m_string = URLEncoder.encode(imdbID,"UTF-8");
+            m_url = "http://www.omdbapi.com/?i="+imdbID+"&y=&plot=short&r=json";
+
+
+        }catch(UnsupportedEncodingException e){
+            e.printStackTrace();
+            Toast.makeText(this, "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
         }
+
+        AsyncHttpClient m_asynclient = new AsyncHttpClient();
+        // Let's display the progress bar right after we created the http call
+        m_Dialog.show();
+        m_asynclient.get(m_url, new JsonHttpResponseHandler(){
+                    @Override
+                    public void onSuccess(JSONObject jsonObject) {
+
+                        Toast.makeText(getApplicationContext(), "Success!", Toast.LENGTH_LONG).show();
+                        Log.d("myimdb", jsonObject.toString());
+
+                        // update the data in your custom method.
+                        m_Dialog.dismiss();
+                        // TODO Add code for the detailed view JSONAdapter
+                        //mJSONAdapter.updateData(jsonObject.optJSONArray("Search"));
+                        Toast.makeText(getApplicationContext(),"details is"+jsonObject.toString(),Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Throwable throwable, JSONObject error) {
+
+                        Toast.makeText(getApplicationContext(), "failed with!"+throwable.getMessage(), Toast.LENGTH_LONG).show();
+                        Log.e("myimdb", statusCode + " " + throwable.getMessage());
+                    }
+                }
 
         );
 
